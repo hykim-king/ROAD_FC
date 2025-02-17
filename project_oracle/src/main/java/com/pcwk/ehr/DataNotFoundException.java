fc63d46b797b0443dc5fd014a582ce85ee19c0c5
kcho7511 <chokyuhee25@gmail.com>
2025-02-17 15:30:43 +0900

    sts git 연동

---
+++ project_oracle/src/main/java/com/pcwk/ehr/DataNotFoundException.java
@@ -0,0 +1,10 @@
+package com.pcwk.ehr;
+
+public class DataNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 274464773246046L;
+	
+	public void DateNotFoundException() {
+		
+	}
+}
