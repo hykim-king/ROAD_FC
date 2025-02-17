ef1df3566cc515373db109a9c351e08ccf3c161e
youngwoo <hotzuri1003@gmail.com>
2025-02-17 16:49:09 +0900

    youngwoo branch first commit

---
+++ src/main/java/com/pcwk/ehr/member/Member.java
@@ -0,0 +1,39 @@
+package com.pcwk.ehr.member;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.ColumnDefault;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Member {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;      //유저 생성id 값
+	
+	@Column(unique = true)
+	private String username;   //userId
+	
+	private String password;   //비밀번호
+	
+	@ColumnDefault(value = "0")
+	private int userGrade;   //유저 구분
+	
+	@Column(unique = true)   // 이메일
+	private String email;
+	
+	private String userDisName;  // 유저 이름
+	
+	private LocalDateTime userRegDt; // 생성일
+	
+}
