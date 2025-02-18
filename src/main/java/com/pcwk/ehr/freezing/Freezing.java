2e1b1e45ad2b720bcc70466d39f4a529484dd765
kcho7511 <chokyuhee25@gmail.com>
2025-02-18 16:46:05 +0900

    modify application.properties

--- src/main/java/com/pcwk/ehr/freezing/Freezing.java
+++ src/main/java/com/pcwk/ehr/freezing/Freezing.java
@@ -3,10 +3,18 @@ package com.pcwk.ehr.freezing;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Entity
+@Table(name = "FREEZING_INFO")
 public class Freezing {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
