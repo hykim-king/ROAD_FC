a817b213976a878df63b7049ce9b533b3475a882
kcho7511 <chokyuhee25@gmail.com>
2025-02-18 16:36:10 +0900

    add Freezing

---
+++ src/main/java/com/pcwk/ehr/freezing/Freezing.java
@@ -0,0 +1,36 @@
+package com.pcwk.ehr.freezing;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Freezing {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "FREEZING_ID", columnDefinition = "NUMBER")
+	private Integer freezingId;
+	
+	@Column(name = "FREEZING_AGENCY", columnDefinition = "VARCHAR2(50 CHAR)")
+	private String freezingAgency;
+	
+	@Column(name = "FREEZING_LENGTH", columnDefinition = "NUMBER")
+	private Integer freezingLength;
+	
+	@Column(name = "FREEZING_START_LAT", columnDefinition = "NUMBER")
+	private Integer freezingStartLat;
+	
+	@Column(name = "FREEZING_START_LON", columnDefinition = "NUMBER")
+	private Integer freezingStartLon;
+	
+	@Column(name = "FREEZING_END_LAT", columnDefinition = "NUMBER")
+	private Integer freezingEndLat;
+	
+	@Column(name = "FREEZING_END_LON", columnDefinition = "NUMBER")
+	private Integer freezingEndLon;
+	
+	@Column(name = "FREEZING_REG_DT", columnDefinition = "DATE")
+	private LocalDateTime freezingRegDt;
+}
