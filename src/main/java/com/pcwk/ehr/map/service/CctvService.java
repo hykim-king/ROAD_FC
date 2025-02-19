7f5ccbe4eeb232a4a0caf8aa253723eb250488b0
yongjin1004 <yongjin7660@gmail.com>
2025-02-19 22:13:33 +0900

    map

---
+++ src/main/java/com/pcwk/ehr/map/service/CctvService.java
@@ -0,0 +1,23 @@
+package com.pcwk.ehr.map.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pcwk.ehr.map.Entity.Cctv;
+import com.pcwk.ehr.map.Repository.CctvRepository;
+
+@Service
+public class CctvService {
+	
+	@Autowired
+	CctvRepository cctvRepository;
+	
+	
+	public List<Cctv> list(){
+		
+		return cctvRepository.findAll();
+	}
+	
+}
