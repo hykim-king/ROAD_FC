04d46704a8aa204f596ef53a13b5266cdf206a9f
kcho7511 <chokyuhee25@gmail.com>
2025-02-21 15:41:21 +0900

    add Map package

---
+++ src/main/java/com/pcwk/ehr/Map/MapController.java
@@ -0,0 +1,5 @@
+package com.pcwk.ehr.Map;
+
+public class MapController {
+	
+}
---
+++ src/main/java/com/pcwk/ehr/freezing/FreezingRepository.java
@@ -0,0 +1,7 @@
+package com.pcwk.ehr.freezing;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FreezingRepository extends JpaRepository<Freezing, Integer> {
+	
+}
